7879c7769d95c2b49affae8644b484549b8666b8
Boris Pokidov <boris86msk@yandex.ru>
2024-12-18 11:37:41 +0300

    add quantity of interviews

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -6,12 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.site.dto.CategoryDTO;
 import ru.job4j.site.service.AuthService;
 import ru.job4j.site.service.CategoriesService;
 import ru.job4j.site.service.InterviewsService;
 import ru.job4j.site.service.NotificationService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
@@ -30,7 +32,17 @@ public class IndexController {
                 "Главная", "/"
         );
         try {
-            model.addAttribute("categories", categoriesService.getMostPopular());
+            var mostPopular = categoriesService.getMostPopular();
+            var listId = mostPopular.stream()
+                    .map(CategoryDTO::getId)
+                    .toList();
+            var interviewsByCategory = new HashMap<>();
+            for (Integer id : listId) {
+                interviewsByCategory.put(id, interviewsService.getByTopicId(id, 0, 0));
+            }
+
+            model.addAttribute("interviews", interviewsByCategory);
+            model.addAttribute("categories", mostPopular);
             var token = getToken(req);
             if (token != null) {
                 var userInfo = authService.userInfo(token);
